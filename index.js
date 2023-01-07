bcc6d1a3399322e53650d8104a925d131ec681f2
Eri Alfonso <thecheese.gw2@gmail.com>
2023-01-07 01:01:27 -0300

    test

---
+++ index.js
@@ -0,0 +1,14 @@
+alert ('Vamos a crear un personaje de Dungeons and Dragons!')
+// do
+let nombrePersonaje = prompt('Escriba el nombre de su personaje')
+console.log ('el nombre de su personaje es ' + nombrePersonaje)
+let razaPersonaje = prompt('Elija la raza de ' + nombrePersonaje)
+console.log ('la raza de ' +nombrePersonaje +' es ' + razaPersonaje)
+
+
+
+
+
+
+// respuesta = prompt ('Quieres crear otro personaje? Escribe no para salir.')
+// }while (respuesta.toLowerCase() != "no")
