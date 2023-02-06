78946db70cfc3dc3d00abeaaad0b5ed5005014ec
Eri Alfonso <thecheese.gw2@gmail.com>
2023-02-06 17:41:22 -0300

    added cards

--- JS/form.js
+++ JS/form.js
@@ -1,3 +1,16 @@
+
+// capturas de dom
+let modalGuardar = document.getElementById("modalGuardar")
+// form
+let personajeBtn = document.getElementById("nuevoPersonaje")
+let personajeFormBox = document.getElementById("personajeNuevoForm")
+
+//ternario para abrir y cerrar el form
+personajeBtn.onclick = function () {
+  personajeFormBox.style.display = personajeFormBox.style.display === "block" ? "none" : "block";
+}
+
+// JS pre armado del form
 const navigateToFormStep = (stepNumber) => {
 document.querySelectorAll(".form-step").forEach((formStepElement) => {
       formStepElement.classList.add("d-none");
@@ -25,6 +38,47 @@ document.querySelectorAll(".btn-navigate-form-step").forEach((formNavigationBtn)
   });
 });
 
+//creadores de personaje
+// form
+const form = document.getElementById('personajeSetupForm')
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    let nombrePersonaje =document.getElementById("nombrePersonaje").value;
+    let clasePersonaje =document.getElementById("clasePersonaje").value;
+    let razaPersonaje =document.getElementById("razaPersonaje").value;
+    let fuerzaTotal =document.getElementById("fuerzaTotal").value;
+    let carismaTotal =document.getElementById("carismaTotal").value;
+    let constitucionTotal =document.getElementById("constitucionTotal").value;
+    let destrezaTotal =document.getElementById("destrezaTotal").value;
+    let inteligenciaTotal =document.getElementById("inteligenciaTotal").value;
+    let sabiduriaTotal =document.getElementById("sabiduriaTotal").value;
+
+    const personajeNew = new Personaje(grupoPersonajes.length + 1, nombrePersonaje, clasePersonaje, razaPersonaje, fuerzaTotal, carismaTotal, constitucionTotal, destrezaTotal, inteligenciaTotal, sabiduriaTotal)
+    console.log(personajeNew)
+    grupoPersonajes.push(personajeNew)
+    console.log(grupoPersonajes)
+    localStorage.setItem("grupoPersonaje", JSON.stringify(grupoPersonajes))
+})
+// // modal
+const modal = document.getElementById('modal-body')
+modalGuardar.addEventListener('click', (event) => {
+    event.preventDefault()
+    let nombrePersonaje =document.getElementById("nombrePersonajeModal").value;
+    let clasePersonaje =document.getElementById("clasePersonajeModal").value;
+    let razaPersonaje =document.getElementById("razaPersonajeModal").value;
+    let fuerzaTotal = Math.floor((Math.random() * 18) + 1);
+    let carismaTotal = Math.floor((Math.random() * 18) + 1);
+    let constitucionTotal = Math.floor((Math.random() * 18) + 1);
+    let destrezaTotal = Math.floor((Math.random() * 18) + 1);
+    let inteligenciaTotal = Math.floor((Math.random() * 18) + 1);
+    let sabiduriaTotal = Math.floor((Math.random() * 18) + 1);
+
+    const personajeNew = new Personaje(grupoPersonajes.length + 1, nombrePersonaje, clasePersonaje, razaPersonaje, fuerzaTotal, carismaTotal, constitucionTotal, destrezaTotal, inteligenciaTotal, sabiduriaTotal)
+    console.log(personajeNew)
+    grupoPersonajes.push(personajeNew)
+    console.log(grupoPersonajes)
+    localStorage.setItem("grupoPersonaje", JSON.stringify(grupoPersonajes))
+})
 
 // contador de puntos
 let counterValue = document.getElementById("counterValue");
@@ -42,3 +96,34 @@ numberInputs.forEach(input => {
     counterValue.innerHTML = counter;
   });
 });
+
+// counter
+function checkNumbers() {
+  let counter = 27;
+  if (fuerzaTotal >= 8 && fuerzaTotal <= 15 &&
+      carismaTotal >= 8 && carismaTotal <= 15 &&
+      constitucionTotal >= 8 && constitucionTotal <= 15 &&
+      destrezaTotal >= 8 && destrezaTotal <= 15 &&
+      inteligenciaTotal >= 8 && inteligenciaTotal <= 15 &&
+      sabiduriaTotal >= 8 && sabiduriaTotal <= 15) {
+
+    counter -= fuerzaTotal > 8 ? (fuerzaTotal > 13 ? 2 : 1) : 0;
+    counter -= carismaTotal > 8 ? (carismaTotal > 13 ? 2 : 1) : 0;
+    counter -= constitucionTotal > 8 ? (constitucionTotal > 13 ? 2 : 1) : 0;
+    counter -= destrezaTotal > 8 ? (destrezaTotal > 13 ? 2 : 1) : 0;
+    counter -= inteligenciaTotal > 8 ? (inteligenciaTotal > 13 ? 2 : 1) : 0;
+    counter -= sabiduriaTotal > 8 ? (sabiduriaTotal > 13 ? 2 : 1) : 0;
+
+    if (counter >= 0) {
+      localStorage.setItem("fuerza", fuerzaTotal);
+      localStorage.setItem("carisma", carismaTotal);
+      localStorage.setItem("constitucion", constitucionTotal);
+      localStorage.setItem("destreza", destrezaTotal);
+      localStorage.setItem("inteligencia", inteligenciaTotal);
+      localStorage.setItem("sabiduria", sabiduriaTotal);
+      ;
+    } else {
+      alert("El total no puede exceder a 27 puntos");
+    }
+  }
+}
--- JS/main.js
+++ JS/main.js
@@ -1,77 +1,35 @@
-// capturas de dom
-// atributos del object
+// capturas de DOM
+let btnVerCatalogo = document.getElementById("verCatalogo")
+let personajesCreados = document.getElementById("personajesCreados")
 
-// form
-let personajeBtn = document.getElementById("nuevoPersonaje")
-let personajeFormBox = document.getElementById("personajeNuevoForm")
 
-//ternario para abrir y cerrar el form
-personajeBtn.onclick = function () {
-    personajeFormBox.style.display = personajeFormBox.style.display === "block" ? "none" : "block";
-}
-// //creador de personaje
-// function nuevoPersonaje(array){
-//     const personajeNew = new Personaje(grupoPersonajes.length + 1, nombrePersonaje.value, clasePersonaje.value, razaPersonaje.value, fuerzaTotal.value, carismaTotal.value, constitucionTotal, destrezaTotal.value, inteligenciaTotal.value, sabiduriaTotal.value)
-//     console.log(personajeNew)
-//     array.push(personajeNew)
-//     console.log(array)
-//     localStorage.setItem("grupoPersonaje", JSON.stringify(array))
-//     // reset de valores
-//     nombrePersonaje.value = ""
-//     clasePersonaje.value = ""
-//     razaPersonaje.value = ""
-//     fuerzaTotal.value = ""
-//     carismaTotal.value = ""
-//     constitucionTotal.value = ""   
-//     destrezaTotal.value = ""
-//     inteligenciaTotal.value = ""
-//     sabiduriaTotal.value = ""
-// }
-const form = document.getElementById('userAccountSetupForm')
-form.addEventListener('submit', (event) => {
-    event.preventDefault()
-    let nombrePersonaje =document.getElementById("nombrePersonaje").value;
-    let clasePersonaje =document.getElementById("clasePersonaje").value;
-    let razaPersonaje =document.getElementById("razaPersonaje").value;
-    let fuerzaTotal =document.getElementById("fuerzaTotal").value;
-    let carismaTotal =document.getElementById("carismaTotal").value;
-    let constitucionTotal =document.getElementById("constitucionTotal").value;
-    let destrezaTotal =document.getElementById("destrezaTotal").value;
-    let inteligenciaTotal =document.getElementById("inteligenciaTotal").value;
-    let sabiduriaTotal =document.getElementById("sabiduriaTotal").value;
-
-    const personajeNew = new Personaje(grupoPersonajes.length + 1, nombrePersonaje, clasePersonaje, razaPersonaje, fuerzaTotal, carismaTotal, constitucionTotal, destrezaTotal, inteligenciaTotal, sabiduriaTotal)
-    console.log(personajeNew)
-    grupoPersonajes.push(personajeNew)
-    console.log(grupoPersonajes)
-})
-
-function checkNumbers() {
-  let counter = 27;
-  if (fuerzaTotal >= 8 && fuerzaTotal <= 15 &&
-      carismaTotal >= 8 && carismaTotal <= 15 &&
-      constitucionTotal >= 8 && constitucionTotal <= 15 &&
-      destrezaTotal >= 8 && destrezaTotal <= 15 &&
-      inteligenciaTotal >= 8 && inteligenciaTotal <= 15 &&
-      sabiduriaTotal >= 8 && sabiduriaTotal <= 15) {
-
-    counter -= fuerzaTotal > 8 ? (fuerzaTotal > 13 ? 2 : 1) : 0;
-    counter -= carismaTotal > 8 ? (carismaTotal > 13 ? 2 : 1) : 0;
-    counter -= constitucionTotal > 8 ? (constitucionTotal > 13 ? 2 : 1) : 0;
-    counter -= destrezaTotal > 8 ? (destrezaTotal > 13 ? 2 : 1) : 0;
-    counter -= inteligenciaTotal > 8 ? (inteligenciaTotal > 13 ? 2 : 1) : 0;
-    counter -= sabiduriaTotal > 8 ? (sabiduriaTotal > 13 ? 2 : 1) : 0;
-
-    if (counter >= 0) {
-      localStorage.setItem("fuerza", fuerzaTotal);
-      localStorage.setItem("carisma", carismaTotal);
-      localStorage.setItem("constitucion", constitucionTotal);
-      localStorage.setItem("destreza", destrezaTotal);
-      localStorage.setItem("inteligencia", inteligenciaTotal);
-      localStorage.setItem("sabiduria", sabiduriaTotal);
-      ;
-    } else {
-      alert("El total no puede exceder a 27 puntos");
-    }
-  }
-}
+// ver personajes function
+function verPersonajes(array){
+  //vaciar Div
+  personajesCreados.innerHTML = ""
+  //tenemos nuestros libros en estanteria:
+  for(let personaje of grupoPersonajes){
+      let verCards = document.createElement("div")
+      //otra forma de sumarle una class a un elemento html
+      //classList + add agrego clases al elemento seleccionado
+      verCards.classList.add("col-12", "col-md-6", "col-lg-4", "mb-3")
+      verCards.innerHTML = `
+      <div id="${personaje.id}" class="card" style="width: 18rem;">
+              <img class="card-img-top img-fluid" style="height: 200px;"src="assets/${personaje.nombre}" alt="${personaje.nombre} el ${personaje.clase}">
+              <div class="card-body">
+                  <h4 class="card-title">${personaje.nombre}</h4>
+                  <p>Autor: ${personaje.clase}</p>
+                  <p class="">Precio: ${personaje.raza}</p>
+              <button id="agregarBtn${personaje.id}" class="btn btn-outline-success">Agregar al carrito</button>
+              </div>
+      </div>
+      `
+      personajesCreados.appendChild(verCards)
+      //captura agregarBtn
+      let agregarBtn = document.getElementById(`agregarBtn${personaje.id}`)
+      //adjunto evento
+      agregarBtn.addEventListener("click", ()=>{
+          
+      })
+  }}
+  btnVerCatalogo.onclick = ()=>{verPersonajes(grupoPersonajes)}
